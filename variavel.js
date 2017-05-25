eb8472f62c60374aab30bec80bf7ff5c6e86ebaf
Alex Fabiano <afabiano15@gmail.com>
2017-05-25 10:29:42 -0400

    criando variavel.js

---
+++ variavel.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+//declaração de constantes
+const naoPodeMudar = 'o valor';
+const vaiLancarErro; // é obrigatório atribuir o valor às constantes
+                     // assim que as declaramos
+
+
+//declaração de variáveis
+let naoInicializada; // possui valor undefined
+let dica = 'JavaScript não é "fortemente tipado"';
+
+var naoInicializada2; // possui valor undefined
+var dica2 = 'Use "let" ao invés de "var"';
